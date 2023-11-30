c91e96f987e695938d2f3abf5506d750028c44ec
luautech <fabricio.santos@luau.tech>
2023-11-30 10:12:13 -0300

    updating

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -9,8 +9,18 @@ const Footer = () => (
     <Row>
       <Col md="12">
         <p>
-          <Translate contentKey="footer">Your footer</Translate>
+          <Translate contentKey="footer">Your Salon's Footer</Translate>
         </p>
+        <div className="footer-links">
+          <a href="/contact">
+            <Translate contentKey="footer.contact">Contact</Translate>
+          </a>
+          <a href="/about">
+            <Translate contentKey="footer.about">About Us</Translate>
+          </a>
+          // Outros links úteis
+        </div>
+        <div className="social-media-icons">// Ícones de redes sociais</div>
       </Col>
     </Row>
   </div>
